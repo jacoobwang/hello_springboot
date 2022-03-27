e02392859b8e08d0e205bd7561dfd3202588ac84
wangyong146 <wangyong7@jd.com>
2022-03-27 12:22:09 +0800

    init

---
+++ src/main/java/com/walker/Application.java
@@ -0,0 +1,16 @@
+package com.walker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        // 运行spring应用
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/walker/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.walker.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String Hello() {
+        return "Hello Spring Boot";
+    }
+}
